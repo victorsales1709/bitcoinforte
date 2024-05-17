7f6eed0b7e37b406e1c439bbcc27d1cc0c86c800
victorsales1709 <bvictorsales@gmail.com>
2024-05-16 22:13:47 -0300

    Concluindo e personalizando a header e a main

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('secaoSelect').addEventListener('change', function() {
-    var selectedSection = this.value;
-    if (selectedSection) {
-        var sectionElement = document.getElementById(selectedSection);
-        if (sectionElement) {
-            sectionElement.scrollIntoView({ behavior: 'smooth' });
-        }
-    }
-});
